debf8600135f7f0ce760f847b11c3978f827151c
Veselin Dimitrov <allowed098@gmail.com>
2023-07-28 21:38:06 +0300

    Add project files.

---
+++ Foodery.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Foodery.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
