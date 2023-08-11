a11af1ec3a5d926dd1c1f204151eca3853762cd7
Veselin Dimitrov <allowed098@gmail.com>
2023-08-11 21:13:08 +0300

    Added button in layout for products page

--- Foodery.Data/Configurations/ProductEntityConfiguration.cs
+++ Foodery.Data/Configurations/ProductEntityConfiguration.cs
@@ -30,7 +30,7 @@
                 Description = "Freshly picked cucumbers from a home garden.",
                 CategoryId = 1,
                 Price = 2.39M,
-                ImageUrl = "https://www.freepik.com/free-photo/green-cucumber_7399053.htm#query=cucumbers&position=0&from_view=search&track=sph"
+                ImageUrl = "https://img.freepik.com/free-photo/green-cucumber_144627-21625.jpg?w=1060&t=st=1691777180~exp=1691777780~hmac=69e89a3b2e1153169d0963bf34f111254a986af1488ef705d28019f8d50b585c"
             };
             products.Add(product);
 
